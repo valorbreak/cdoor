8f722c8303a9d38fb7210d7097688cae2b26dfe8
LeslieUri <valorbreak@gmail.com>
2014-12-07 21:12:46 -0500

    cdoor - final

--- js/main.js
+++ js/main.js
@@ -19,3 +19,48 @@ $(function() {
         drawBlur();
     }
 });
+
+
+
+
+$(document).ready(function(){
+
+    // Snow
+    $.fn.snow({
+        minSize: 5, // Minimumsize of the snowflake (px)
+        maxSize: 25, // Maximum size of the snowflake (px)
+        newOn: 350, // Frequency they fall (ms)
+        flakeColor: '#FFF' // Colour of the snowflake
+    });
+
+
+    // Rotating Images
+
+    var images =
+        [
+            'assets/img/Boston.jpg',
+            'assets/img/christmas-hd-wallpaper-vorjrqbx.jpg',
+            'assets/img/druplicon.small_.png',
+            'assets/img/inconceivable.jpg'
+        ];
+
+    $('img.rotatingImage').attr({"src": images[Math.floor(Math.random() * images.length)]});
+
+    function logoTextChange(){
+
+        var headerColors = [
+          '#3498DB',
+          '#E74C3C',
+          '#82BF56',
+          '#F2CF66',
+          '#FFF',
+        ];
+        $('.rotateColor1').css({"color": headerColors[Math.floor(Math.random() * images.length)]});
+        $('.rotateColor2').css({"color": headerColors[Math.floor(Math.random() * images.length)]});
+        $('.rotateColor3').css({"color": headerColors[Math.floor(Math.random() * images.length)]});
+    }
+
+    setInterval(logoTextChange, 1500);
+});
+
+
